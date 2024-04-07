79f1d3ae6aaa5f8fedba65debc0b80ec3580186b
Téo Bouvard <teobouvard@gmail.com>
2024-04-07 17:59:25 +0200

    Extract takeoff time from track

--- src/flight.rs
+++ src/flight.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use chrono::NaiveDate;
+use chrono::{DateTime, NaiveDate, NaiveDateTime, Utc};
 use geojson::GeoJson;
 use geojson::Value::LineString;
 use serde::Serialize;
@@ -11,15 +11,18 @@ use crate::igc::{IgcFile, IgcFix};
 #[derive(Serialize)]
 pub struct Flight {
     pub date: NaiveDate,
+    pub takeoff: DateTime<Utc>,
     pub geojson: GeoJson,
     pub duration: Duration,
     pub track_duration: Duration,
 }
 
 impl Flight {
+    const MIN_FLYING_SPEED: f64 = 2.0;
     pub fn new(track: IgcFile) -> Self {
         Flight {
             date: track.get_date().expect("Missing date header"),
+            takeoff: Flight::takeoff(&track),
             geojson: Flight::geojson(&track),
             duration: Flight::duration(&track),
             track_duration: track.duration(),
@@ -37,14 +40,32 @@ impl Flight {
         .into()
     }
 
+    pub fn takeoff(track: &IgcFile) -> DateTime<Utc> {
+        DateTime::from_naive_utc_and_offset(
+            NaiveDateTime::new(
+                track.get_date().unwrap(),
+                track
+                    .fixes
+                    .windows(2)
+                    .find_map(|pair| match pair {
+                        [a, b] if Flight::speed_on_trajectory(a, b) > Flight::MIN_FLYING_SPEED => {
+                            Some(a.ts)
+                        }
+                        _ => None,
+                    })
+                    .unwrap_or(track.fixes.first().unwrap().ts),
+            ),
+            Utc,
+        )
+    }
+
     pub fn duration(track: &IgcFile) -> Duration {
-        const MIN_FLYING_SPEED: f64 = 2.0;
         Duration::from_seconds(
             track
                 .fixes
                 .windows(2)
                 .map(|pair| match pair {
-                    [a, b] if Flight::speed_on_trajectory(a, b) > MIN_FLYING_SPEED => {
+                    [a, b] if Flight::speed_on_trajectory(a, b) > Flight::MIN_FLYING_SPEED => {
                         (b.ts - a.ts).num_seconds()
                     }
                     _ => 0,
