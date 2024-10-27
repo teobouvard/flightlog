c33d069b4ce26262da6c0051dc9a47edee8b6ff8
Téo Bouvard <teobouvard@gmail.com>
2024-10-27 11:57:27 +0100

    Split FlightMap CSS

--- web/src/pages/FlightMap.jsx
+++ web/src/pages/FlightMap.jsx
@@ -4,6 +4,7 @@ import maplibregl from "maplibre-gl";
 import PropTypes from "prop-types";
 import { useEffect, useRef } from "react";
 
+import "./FlightMap.css";
 import { mapStyle, skyStyle } from "./MapStyle";
 
 function FlightMap({ flight, range }) {
