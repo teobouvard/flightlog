6e66bf97d25375fc5998edf7f87c47ac53b55bf5
Téo Bouvard <teobouvard@gmail.com>
2024-10-29 21:16:42 +0100

    Fix GitHub Pages compatibility issues

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 
 import FlightDetailsPage from "./pages/FlightDetailsPage";
 import IndexPage from "./pages/IndexPage";
